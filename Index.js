f96d4163e1fb6f8abdb1134a06cce3d330e13ab4
harry <harrison.court25@gmail.com>
2022-07-11 05:22:00 +0930

    Testing to see if viewport height would be beneficial
    + Added list into Index.js that will be worked on later.

--- Index.js
+++ Index.js
@@ -1,2 +1,9 @@
-// Test Function.
+// To all users who stumble across this, you can add your own sayings here!
+var reasons = [
+    "You've survived every bad day that you've ever had.", 
+    "Your friends will miss you.", 
+    "You still have so much potential."];
+
+
+
 document.getElementById("test").innerHTML = "Tell me more..."
